fe4af77e7cbbe06661e5daa08d3d8c00c9d469da
Sanchit <sanch@example.com>
2026-07-31 14:56:27 +0530

    Day 15

--- Day_15.py
+++ Day_15.py
@@ -2,18 +2,15 @@ from random import randint
 
 c = randint(1, 100)
 
-n = int(input("Enter a number between 1 and 100: "))
-
 guesses = 0
 
 while True:
     guesses +=1
+    n = int(input("Enter a number between 1 and 100: "))
     if n< c:
         print("Try Higher!")
-        n = int(input("Enter a number between 1 and 100: "))
     elif n > c:
         print("Try Lower!")
-        n = int(input("Enter a number between 1 and 100: "))
     else:
-        print(f"Congratulations! You guessed the number in {guesses} guesses.")
+        print(f"Congratulations! You guessed the number {n} in {guesses} guesses.")
         break
